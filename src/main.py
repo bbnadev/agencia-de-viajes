c546af4585a82977231a1691ed8fd4944b0390ec
bbnadev <nicolas.bahamondeb@gmail.com>
2024-12-13 09:16:12 -0300

    Agregar vistas de usuario para gestionar paquetes turísticos y reservas, incluyendo opciones para listar, reservar y eliminar reservas

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ from views.admin.AdmPaquetesView import AdmPaquetesView
 from views.menus import MENUS, print_menu, clear_screen, sleep
 from database.database import Database
 
+from views.user.UserPaquetesView import UserPaquetesView
+from views.user.UserReservasView import UserReservasView
+
 
 def main():
     user = None
@@ -21,12 +24,13 @@ def main():
                         clear_screen()
                         print("[*] Saliendo...")
                         break
+                    case _:
+                        clear_screen()
             except KeyboardInterrupt:
                 clear_screen()
                 print("[*] Saliendo...")
                 break
         else:
-            clear_screen()
             match user.get_rol():
                 case 1:
                     print_menu(MENUS.get("admin"), 'MENÚ ADMINISTRADOR')
@@ -51,6 +55,8 @@ def main():
                                             print(
                                                 "[*] Volviendo al menú principal...")
                                             break
+                                        case _:
+                                            clear_screen()
                             case "2":
                                 admPaquetesView = AdmPaquetesView()
                                 while True:
@@ -70,10 +76,14 @@ def main():
                                             print(
                                                 "[*] Volviendo al menú principal...")
                                             break
+                                        case _:
+                                            clear_screen()
                             case "q":
                                 clear_screen()
                                 print("[*] Saliendo...")
                                 break
+                            case _:
+                                clear_screen()
                     except Exception as err:
                         print(f'[!] Ocurrió un error: {err}')
                         continue
@@ -86,11 +96,39 @@ def main():
                     try:
                         match input("> "):
                             case "1":
-                                print("Paquetes Turísticos")
+                                userPaquetesView = UserPaquetesView()
+                                userReservasView = UserReservasView()
+                                while True:
+                                    print_menu(MENUS.get("user.paquetes"),
+                                               'MENÚ PAQUETES TURÍSTICOS')
+                                    match input("> "):
+                                        case "1":
+                                            userPaquetesView.listar()
+                                        case "2":
+                                            userPaquetesView.listar_por_fecha()
+                                        case "3":
+                                            userReservasView.reservar(
+                                                user.get_id())
+                                        case "q":
+                                            clear_screen()
+                                            print(
+                                                "[*] Volviendo al menú principal...")
+                                            break
+                                        case _:
+                                            clear_screen()
+                            case "2":
+                                userReservasView = UserReservasView()
+                                userReservasView.listar(user.get_id())
+                            case "3":
+                                userReservasView = UserReservasView()
+                                userReservasView.eliminar_reserva(
+                                    user.get_id())
                             case "q":
                                 clear_screen()
                                 print("[*] Saliendo...")
                                 break
+                            case _:
+                                clear_screen()
                     except Exception as err:
                         print(f'[!] Ocurrió un error: {err}')
                         continue
