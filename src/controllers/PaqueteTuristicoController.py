fc71206f72d07df788ae48c3671546a67564618e
bbnadev <nicolas.bahamondeb@gmail.com>
2024-12-13 09:16:08 -0300

    Agregar método para obtener paquetes turísticos por rango de fechas y mejorar manejo de errores en el controlador

--- src/controllers/PaqueteTuristicoController.py
+++ src/controllers/PaqueteTuristicoController.py
@@ -1,6 +1,7 @@
 from database.database import Database
 from models.Actividad import Actividad
 from models.PaqueteTuristico import PaqueteTuristico, Destino
+from datetime import date
 
 
 class PaqueteTuristicoController():
@@ -58,7 +59,7 @@ class PaqueteTuristicoController():
             return list(paquetes_dict.values())
         except Exception as e:
             print(
-                f"(Controller) Ocurrio un error al intentar listar los paquetes, destinos y actividades: {e}")
+                f"(Controller) Ocurrio un error al intentar listar los paquetes y destinos: {e}")
             return []
 
     def costo_total(self, paquete_id: int) -> float:
@@ -180,3 +181,96 @@ class PaqueteTuristicoController():
             print(
                 f"(Controller) Ocurrio un error al intentar verificar la existencia del paquete: {e}")
             return False
+
+    def obtener_paquetes_x_fechas(self, fecha_rango_inicial: date, fecha_rango_fin: date) -> list[PaqueteTuristico]:
+        query = """SELECT
+                pt.id AS paquete_id, pt.nombre AS paquete_nombre, pt.fechaInicio, pt.fechaFin,
+                d.id AS destino_id, d.nombre AS destino_nombre, d.descripcion AS destino_descripcion, d.costo
+            FROM
+                PaqueteTuristico pt
+            LEFT JOIN
+                PaqueteXDestino pxd ON pt.id = pxd.paqueteId
+            LEFT JOIN
+                Destino d ON pxd.destinoId = d.id
+            WHERE
+                pt.fechaInicio BETWEEN %s AND %s
+            ORDER BY paquete_id;"""
+        try:
+            conn = self.db.connect()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query, (fecha_rango_inicial, fecha_rango_fin))
+            filas = cursor.fetchall()
+            if not filas:
+                return None
+            paquetes_dict = {}
+            for fila in filas:
+                paquete_id = fila['paquete_id']
+                if paquete_id not in paquetes_dict:
+                    paquete = PaqueteTuristico(id=paquete_id, nombre=fila['paquete_nombre'],
+                                               fechaInicio=fila['fechaInicio'], fechaFin=fila['fechaFin'])
+                    paquetes_dict[paquete_id] = paquete
+                destino_id = fila['destino_id']
+                if destino_id:
+                    if destino_id not in paquetes_dict[paquete_id].get_destinos():
+                        destino = Destino(id=destino_id, nombre=fila['destino_nombre'],
+                                          descripcion=fila['destino_descripcion'], costo=fila['costo'])
+                        paquetes_dict[paquete_id].add_destino(destino)
+
+                # if fila['actividad_id']:
+                #     actividad = Actividad(
+                #         id=fila['actividad_id'], nombre=fila['actividad_nombre'], descripcion=fila['actividad_descripcion'])
+                #     paquetes_dict[paquete_id].destinos[destino_id].add_actividad(
+                #         actividad)
+            cursor.close()
+            conn.close()
+            return list(paquetes_dict.values())
+        except Exception as e:
+            print(
+                f"(Controller) Ocurrio un error al intentar listar los paquetes y destinos por fecha: {e}")
+            return []
+
+    def obtener_paquete_x_id(self, paqueteId: int) -> PaqueteTuristico:
+        query = """SELECT
+                pt.id AS paquete_id, pt.nombre AS paquete_nombre, pt.fechaInicio, pt.fechaFin,
+                d.id AS destino_id, d.nombre AS destino_nombre, d.descripcion AS destino_descripcion, d.costo
+            FROM
+                PaqueteTuristico pt
+            LEFT JOIN
+                PaqueteXDestino pxd ON pt.id = pxd.paqueteId
+            LEFT JOIN
+                Destino d ON pxd.destinoId = d.id
+            WHERE pt.id = %s
+            ORDER BY paquete_id;"""
+        try:
+            conn = self.db.connect()
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query, (paqueteId,))
+            filas = cursor.fetchall()
+            if not filas:
+                return None
+            paquetes_dict = {}
+            for fila in filas:
+                paquete_id = fila['paquete_id']
+                if paquete_id not in paquetes_dict:
+                    paquete = PaqueteTuristico(id=paquete_id, nombre=fila['paquete_nombre'],
+                                               fechaInicio=fila['fechaInicio'], fechaFin=fila['fechaFin'])
+                    paquetes_dict[paquete_id] = paquete
+                destino_id = fila['destino_id']
+                if destino_id:
+                    if destino_id not in paquetes_dict[paquete_id].get_destinos():
+                        destino = Destino(id=destino_id, nombre=fila['destino_nombre'],
+                                          descripcion=fila['destino_descripcion'], costo=fila['costo'])
+                        paquetes_dict[paquete_id].add_destino(destino)
+
+                # if fila['actividad_id']:
+                #     actividad = Actividad(
+                #         id=fila['actividad_id'], nombre=fila['actividad_nombre'], descripcion=fila['actividad_descripcion'])
+                #     paquetes_dict[paquete_id].destinos[destino_id].add_actividad(
+                #         actividad)
+            cursor.close()
+            conn.close()
+            return list(paquetes_dict.values())
+        except Exception as e:
+            print(
+                f"(Controller) Ocurrio un error al intentar listar los paquetes y destinos: {e}")
+            return []
